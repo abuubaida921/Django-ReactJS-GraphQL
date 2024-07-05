17430879d43a0e5d3eb148d52aba2239ffaa740b
Abu Ubaida <ubaidanazia@gmail.com>
2024-07-05 09:22:32 +0600

    some package not found error solved

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=150)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.id}--{self.title}"
