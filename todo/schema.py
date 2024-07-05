e6d8516681f65291f5fa769d5271e24affcd5fb4
Abu Ubaida <ubaidanazia@gmail.com>
2024-07-05 14:01:45 +0600

    Searching Todo item according to id

--- todo/schema.py
+++ todo/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from .models import Todo
-from graphene_django import DjangoObjectType,DjangoListField
+from graphene_django import DjangoObjectType
 
 class TodoType(DjangoObjectType):
     class Meta:
@@ -8,6 +8,8 @@ class TodoType(DjangoObjectType):
         fields=('id','title','date')
         
 class Query(graphene.ObjectType):
-    todos= DjangoListField(TodoType)
-    def resolve_todos(self,info):
+    todos= graphene.List(TodoType,id=graphene.Int())
+    def resolve_todos(self,info, id = None):
+        if id:
+            return Todo.objects.filter(id=id)
         return Todo.objects.all()
