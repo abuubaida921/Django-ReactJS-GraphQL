88f9dfa112dc1544464d2a1d800c61bc5a7a9bd8
Abu Ubaida <ubaidanazia@gmail.com>
2024-07-06 06:55:35 +0600

    Add new box implemented as well as some color correction implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useQuery, gql } from "@apollo/client";
-import { Avatar, ListItem, ListItemIcon, ListItemText, Container, Box, List, ListItemSecondaryAction, IconButton} from "@mui/material";
+import { Avatar, ListItem, ListItemIcon, ListItemText, Container, Box, List, ListItemSecondaryAction, IconButton, Typography, TextField, Button} from "@mui/material";
 import React from "react";
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
@@ -19,6 +19,20 @@ const App = () => {
   if(error) return <h1>Error Occurred Lading Data...</h1>;
   return (
     <Container>
+      <Typography align="center" variant="h3">ToDo App</Typography>
+      <Box style={{
+        maxWidth:"500 px",
+        margin:"0 auto",
+        display:"flex"
+      }}>
+        <TextField
+        fullWidth
+        id="outlined-basic"
+        label="Add new todo..."
+        variant="outlined"
+        />
+        <Button variant="contained" color="primary">Add</Button>
+      </Box>
       <Box component="div" style={{
         maxWidth:"500 px",
         margin:"0 auto"
@@ -28,12 +42,14 @@ const App = () => {
             data?.todos?.map((item,i)=>(
               <ListItem button key={i}>
                 <ListItemIcon>
-                  <Avatar>{i+1}</Avatar>
+                  <Avatar style={{
+                    backgroundColor:"blue"
+                  }}>{i+1}</Avatar>
                 </ListItemIcon>
                 <ListItemText primary={item?.title}/>
                 <ListItemSecondaryAction>
-                  <IconButton><EditIcon/></IconButton>
-                  <IconButton><DeleteForeverIcon/></IconButton>
+                  <IconButton><EditIcon color="primary"/></IconButton>
+                  <IconButton><DeleteForeverIcon color="secondary"/></IconButton>
                 </ListItemSecondaryAction>
               </ListItem>
             ))
