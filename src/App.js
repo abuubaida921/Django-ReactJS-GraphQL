4487ae2481bb6abd2c1567a2c39ac754816ce803
Abu Ubaida <ubaidanazia@gmail.com>
2024-07-06 06:40:17 +0600

    Edit and Delete button added with beautiful ListView

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { useQuery, gql } from "@apollo/client";
+import { Avatar, ListItem, ListItemIcon, ListItemText, Container, Box, List, ListItemSecondaryAction, IconButton} from "@mui/material";
 import React from "react";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 const App = () => {
   const{loading,error,data} = useQuery(gql`
@@ -15,9 +18,29 @@ const App = () => {
   if(loading) return <h1>Loading Data...</h1>;
   if(error) return <h1>Error Occurred Lading Data...</h1>;
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Container>
+      <Box component="div" style={{
+        maxWidth:"500 px",
+        margin:"0 auto"
+      }}>
+        <List>
+          {
+            data?.todos?.map((item,i)=>(
+              <ListItem button key={i}>
+                <ListItemIcon>
+                  <Avatar>{i+1}</Avatar>
+                </ListItemIcon>
+                <ListItemText primary={item?.title}/>
+                <ListItemSecondaryAction>
+                  <IconButton><EditIcon/></IconButton>
+                  <IconButton><DeleteForeverIcon/></IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            ))
+          }
+        </List>
+      </Box>
+    </Container>
   );
 };
 
